7913b1d1297dca62c8309c96b5b12053ac6fdb1b
Bhaveshkadam <bhaukadam508@gmail.com>
2023-01-15 20:40:13 +0200

    added file

---
+++ Longest Common Prefix/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public String longestCommonPrefix(String[] strs) {
+        if 9String str = "flow";
+        String str1 = "dog";
+        String prefix = "fi";
+        String prefix1 = "do";
+        boolean result = str.startsWith(prefix);
+        boolean result1 = str1.startsWith(prefix1);
+        System.out.println(" "+ result);
+        return str;
+    }
+}
