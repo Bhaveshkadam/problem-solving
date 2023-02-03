8aedb9c98043ff267c567f1160d4ed4514294938
Bhaveshkadam <bhaukadam508@gmail.com>
2023-02-03 16:18:52 +0530

    reverse string

---
+++ interview-question/reverse a string/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String input = "Geeks for Geeks";
+
+        StringBuilder input1 = new StringBuilder();
+
+        // append a string into StringBuilder input1
+        // TODO: read about StringBuilder
+        input1.append(input);
+
+        // reverse StringBuilder input1
+        input1.reverse();
+
+        System.out.println(input1);
+
+    }
+}
---
+++ interview-question/reverse a string/src/Reverse.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+public class Reverse {
+
+}
